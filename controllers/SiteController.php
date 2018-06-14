b9936adaf1b59f3d28fdf397cdbedc04b8399f07
sutiporn <m_16.k@hotmail.com>
2018-06-14 21:35:29 +0700

    routing yii

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    public function actionHello()
+    {
+        $name = "sutiporn";
+        return $this->render('Hello',array('name'=>$name));
+    }
 }
---
+++ views/site/hello.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sutiporn
+ * Date: 6/14/2018 AD
+ * Time: 9:22 PM
+ */
+echo "hi";
+echo $name;
